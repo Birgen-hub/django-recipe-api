2587e103b11a4c7be27c550e6cb8179ff0191b07
Dolly Birgen <dollybirgen@gmail.com>
2025-12-16 14:59:16 +0300

    feat(accounts): Implemented Register, Login, Logout, and User profile views.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from .views import RegisterView, LoginView, UserView
+from knox import views as knox_views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('user/', UserView.as_view(), name='user-details'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import RegisterSerializer, AuthTokenSerializer
 
-# Create your views here.
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = AuthTokenSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        return Response({
+            "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+class UserView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = RegisterSerializer
+
+    def get_object(self):
+        return self.request.user
