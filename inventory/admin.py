edb7c127959ae763e68546b36d3c497bb29241db
Dolly Birgen <dollybirgen@gmail.com>
2025-12-16 15:13:26 +0300

    chore: Register all models (User, Recipes, Inventory) in Django Admin.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import InventoryItem
 
-# Register your models here.
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ingredient', 'quantity', 'unit')
+    list_filter = ('user',)
+    search_fields = ('ingredient__name',)
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tag, Ingredient, Recipe
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'time_minutes', 'price',)
+    filter_horizontal = ('tags', 'ingredients',)
