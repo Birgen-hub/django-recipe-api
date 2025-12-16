0d29afd64fc5ccbd327ff4317386e589836ae640
Dolly Birgen <dollybirgen@gmail.com>
2025-12-16 16:02:24 +0300

    fix(recipes): Ensure proper inheritance and queryset definition in BaseRecipeAttrViewSet to resolve 500 WrappedAttributeError.

--- recipes/views.py
+++ recipes/views.py
@@ -1,55 +1,84 @@
-from rest_framework import viewsets, mixins, permissions, status
-from rest_framework.authentication import TokenAuthentication
+from rest_framework import viewsets, mixins, permissions, authentication
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Tag, Ingredient, Recipe
-from .serializers import TagSerializer, IngredientSerializer, RecipeReadSerializer, RecipeWriteSerializer
+from .serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailSerializer
 
+# Base viewset for Tag and Ingredient
 class BaseRecipeAttrViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
-    authentication_classes = [TokenAuthentication]
+    """Base viewset for user owned recipe attributes"""
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+    
+    # NOTE: The actual queryset attribute is set in the inheriting classes (TagViewSet, IngredientViewSet)
 
     def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        # This filters the queryset (Tag.objects.all() or Ingredient.objects.all())
+        # to only include items belonging to the current user.
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
+        """Create a new object and assign it to the logged in user"""
         serializer.save(user=self.request.user)
 
+
 class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
+
 class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
 
+
 class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
-        queryset = self.queryset.filter(user=self.request.user).order_by('-id')
-        
+        """Retrieve the recipes for the authenticated user"""
         tags = self.request.query_params.get('tags')
         ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset.filter(user=self.request.user)
 
         if tags:
             tag_ids = self._params_to_ints(tags)
-            queryset = queryset.filter(tags__id__in=tag_ids).distinct()
-        
+            queryset = queryset.filter(tags__id__in=tag_ids)
         if ingredients:
             ingredient_ids = self._params_to_ints(ingredients)
-            queryset = queryset.filter(ingredients__id__in=ingredient_ids).distinct()
-
-        return queryset.prefetch_related('tags', 'ingredients')
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+            
+        return queryset.order_by('-id').distinct()
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
-            return RecipeReadSerializer
-        return RecipeWriteSerializer
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return RecipeDetailSerializer
+        return self.serializer_class
 
     def perform_create(self, serializer):
+        """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        
+        return Response(serializer.errors, status=400)
